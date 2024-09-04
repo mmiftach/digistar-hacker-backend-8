c94bb66c2276edc0fcb92aa7db80b4606afa3caf
mmiftach <940487@telkom.co.id>
2024-09-04 21:15:18 +0700

    modul5

---
+++ database/db.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+const uri = "mongodb://localhost:27017/digistar";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+//function to test connection mongodb
+async function connectDB(){
+    try{
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }catch(err){
+        console.log("MongoDB connection failed",err);
+        process.exit(1);
+    }
+}
+
+async function closeDB(){
+    try{
+        await mongoose.connection.close();
+        console.log("MongoDB connection closed");
+    }catch(err){
+        console.log("MongoDB connection failed",err);
+        process.exit(1);
+    }
+}
+module.exports ={
+    connectDB,
+    closeDB
+};
--- express.js
+++ express.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const  jwt = require('jsonwebtoken');
+const { verifyToken}= require('./middleware/jwt');
 const app = express();
 const PORT = 3000;
+
+
+const db = require('./database/db');
+const query = require('./database/query');
+db.connectDB();
 
 let users = [];
 
@@ -9,60 +17,92 @@ let users = [];
 app.use(bodyParser.json());
 
 // Route to GET all users - returns the users array as JSON
-app.get('/users', (req, res) => {
-  res.json(users);
+app.get('/users',verifyToken, (req, res) => {
+  query.getAllUsers().then((users) => {
+    res.status(200).json(users);
+  }).catch((err) => {
+    res.status(500).json({ message: 'Internal Server Error' });
+  });
 });
 
 // Route to GET a single user by index
-app.get('/users/:index', (req, res) => {
-  const index = parseInt(req.params.index, 10);
-  if (index >= 0 && index < users.length) {
-    res.json(users[index]);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+app.get('/users/:index', verifyToken,(req, res) => {
+  const { index } = req.params; // Extract the id from the request parameters
+  
+  query.getUserById(index).then((user) => {
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  }).catch((err) => {
+    res.status(500).json({ message: 'Internal Server Error' });
+  });
 });
+
 
 // Route to POST a new user - adds a new user to the users array
 app.post('/users', (req, res) => {
-  const user = req.body; // Extract the user from the request body
-  users.push(user); // Add the new user to the users array
-  res.status(201).json(user); // Respond with the created user and status code 201
+  query.createUser(req.body).then((user) => {
+    res.status(201).json(user);
+  }).catch((err) => {
+    res.status(500).json({ message: err.message });
+  });
 });
 
-// Route to PUT (update) a user by index
-app.put('/users/:index', (req, res) => {
-  if (index >= 0 && index < users.length) {
-    users[index] = req.body;
-    res.json(users[index]);
-  } else {
-    res.status(404).json({ message: 'User not found' });
+// Route to POST a new user - adds a new user to the users array
+app.post('/register', async(req, res) => {
+  const user = req.body; // Extract the user from the request body
+  hashPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashPassword;
+  query.createUser(user).then((user) => {
+    res.status(201).json(user); // Respond with the created user and status code 201
+  })
+});
+
+app.post('/login', async(req, res) => {
+  const user =req.body;
+  const validUser = await login(user.username, user.password);
+  res.status(200).json(validUser);
+});
+
+async function login(username, password){
+  const user = await query.searchByUsername(username);
+  valid = await bcrypt.compare(password, user[0].password);
+  if(!valid){
+    throw new Error('Invalid username or password');
   }
+
+  const key = 'secret';
+  const token = jwt.sign({ id: user[0]._id }, key, { expiresIn: '1h' });
+  return { token: token, user: user[0] };
+}
+
+// Route to PUT (update) a user by index
+app.put('/users/:index',verifyToken,(req, res) => {
+  const { index } = req.params; // Extract the id from the request parameters
+  const user = req.body; // Extract the updated user from the request body
+  query.updateUser(index, user).then((user) => {
+    res.status(200).json(user); // Respond with the updated user
+  });
 });
 
 // Route to DELETE a user by index
-app.delete('/users/:index', (req, res) => {
-  const index = parseInt(req.params.index, 10);
-  if (index >= 0 && index < users.length) {
-    users.splice(index, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+app.delete('/users/:index', verifyToken,(req, res) => {
+  const { index } = req.params; // Extract the id from the request parameters
+  query.deleteUser(index).then(() => {
+    res.status(204).send(); // Respond with no content and status code 204
+  });
 });
 
 // Route to search for a user by name
-app.get('/search', (req, res) => {
+app.get('/search', verifyToken,(req, res) => {
   const name = req.query.name;
-  if (!name) {
-    return res.status(400).json({ message: 'Name query parameter is required' });
-  }
-  const user = users.find(u => u.name.toLowerCase() === name.toLowerCase());
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+  query.searchUserByName(name).then((users) => {
+    res.json(users);
+  }).catch((err) => {
+    res.status(500).json({ message: 'Internal Server Error' });
+  });
 });
 
 app.listen(PORT, () => {
